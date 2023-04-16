9ca1de27f4e5d6b51ea8014983a13f66fd0ac414
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2023-04-15 18:41:56 -0700

    Update ActividadDados.java

--- ActividadDados/src/main/java/com/uabc/labs/actividaddados/ActividadDados.java
+++ ActividadDados/src/main/java/com/uabc/labs/actividaddados/ActividadDados.java
@@ -13,13 +13,31 @@ public class ActividadDados {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner scString = new Scanner(System.in);
+        boolean confirmContinue = true;
         HashSet<dado> DadosGuardados = new HashSet<>();
+        do{
         System.out.println("ingrese la cantidad de dados a generar: ");
         int cantidadDados = sc.nextInt();
+        if(cantidadDados>5||cantidadDados<1){
+            System.out.println("error: cantidad invalida");
+        }else{
         for(int k=0;k<cantidadDados;k++){
             dado Dado1 = new dado();
             DadosGuardados.add(Dado1);
         }
         System.out.println(DadosGuardados);
-    }
+        }
+            System.out.println("desea continuar?(si/no):");
+             String confirm = scString.nextLine();
+             if(confirm.equalsIgnoreCase("si")){
+                 confirmContinue =true;
+                 
+             }else if(confirm.equalsIgnoreCase("no")){
+                 confirmContinue = false;
+                 
+             }
+    }while(confirmContinue=false);
+        }
 }
+
