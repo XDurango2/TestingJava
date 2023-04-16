87982ccc1dfaf70bff31e5c4e5054df6d505f2d2
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2023-04-15 18:22:36 -0700

    add ActividadDados
    se incluye la Actividad de dados

--- ActividadDados/src/main/java/com/uabc/labs/actividaddados/ActividadDados.java
+++ ActividadDados/src/main/java/com/uabc/labs/actividaddados/ActividadDados.java
@@ -3,7 +3,8 @@
  */
 
 package com.uabc.labs.actividaddados;
-
+import java.util.HashSet;
+import java.util.Scanner;
 /**
  *
  * @author us
@@ -11,6 +12,14 @@ package com.uabc.labs.actividaddados;
 public class ActividadDados {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        HashSet<dado> DadosGuardados = new HashSet<>();
+        System.out.println("ingrese la cantidad de dados a generar: ");
+        int cantidadDados = sc.nextInt();
+        for(int k=0;k<cantidadDados;k++){
+            dado Dado1 = new dado();
+            DadosGuardados.add(Dado1);
+        }
+        System.out.println(DadosGuardados);
     }
 }
--- ActividadDados/src/main/java/com/uabc/labs/actividaddados/dado.java
+++ ActividadDados/src/main/java/com/uabc/labs/actividaddados/dado.java
@@ -12,13 +12,18 @@ public final class dado {
     private int cantidadLados= 6;
     private int ladoVisible;
 
-    public dado(int ladoVisible) {
+    public dado() {
         getLadoVisible();
     }
     public int getLadoVisible(){
         Random rd = new Random();
-        this.ladoVisible= rd.nextInt(cantidadLados);
+        this.ladoVisible= rd.nextInt(cantidadLados+1);
         return ladoVisible;
+    }
+
+    @Override
+    public String toString() {
+        return "dado{" + "cantidadLados=" + cantidadLados + ", ladoVisible=" + ladoVisible + '}'+"\n";
     }
     
 }
